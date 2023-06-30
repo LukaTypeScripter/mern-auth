7163df45bda9651187679681371a82a65b90f643
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-30 14:25:20 +0400

    added toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Header from './components/Header'
 import {Outlet} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
     <Header/>
+    <ToastContainer/>
     <Container>
     <Outlet/>
     </Container>
