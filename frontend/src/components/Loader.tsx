30f1e257d14de16cfceb060717d177b74018842d
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-30 14:26:02 +0400

    loader

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,14 @@
+import { Spinner } from "react-bootstrap";
+
+import React from 'react'
+
+function Loader() {
+  return (
+    <Spinner animation="border"
+    role="status"
+    style={{width:'100px',height:'100px',margin:'auto',display:'block'}}
+    ></Spinner>
+  )
+}
+
+export default Loader
